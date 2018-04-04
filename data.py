7404c664e7f48b72bcb435683d2d4587ea55882b
Aaron Wong <aronwong@Aarons-MacBook-Pro.local>
2018-04-04 17:59:35 +0800

    first commit

---
+++ data.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+def Articles():
+    articles = [
+        {
+            'id': 1,
+            'appname': 'meitun-front',
+            'body': '',
+            'creator': '',
+            'createtime': '',
+        },
+
+        {
+            'id': 2,
+            'appname': 'item-front',
+            'body': '',
+            'creator': '',
+            'createtime': '',
+        },
+
+        {
+            'id': 3,
+            'appname': 'base-front',
+            'body': '',
+            'creator': '',
+            'createtime': '',
+        }
+
+    ]
+    return articles
